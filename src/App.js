2c74664b4ca021890882b2eb7d249fb2d71dbf9a
StephenReiser <Stephen.Reiser1@gmail.com>
2019-06-02 12:06:50 -0400

    added fetch to pull user id

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login'
 import NewUser from './components/NewUser'
 import Navbar from './components/Navbar'
 
+
 let baseURL = ''
 
 if (process.env.NODE_ENV === 'development') {
@@ -31,6 +32,7 @@ class App extends React.Component {
     this.logOut = this.logOut.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
+    this.getUser = this.getUser.bind(this)
     
   }
   handleChange (event) {
@@ -79,12 +81,18 @@ class App extends React.Component {
     })
     .then(response => response.json())
     .then(function(result) {
-      // console.dir(result)
+      console.dir(result)
       // don't need to see the result
       localStorage.setItem("jwt", result.jwt)
+      return result.jwt
+      
+      // const jwtToken = result.jwt
+      // this.getUser(jwtToken)
       
       
-    }).then(() => {
+    }).then(result => this.getUser(result))
+    
+    .then(() => {
       return this.setState({
         currentUser: email,
         loginEmail: '',
@@ -94,6 +102,40 @@ class App extends React.Component {
   // realistically should set state here to be like incorrect credentials, and then render a new view
   
   
+  }
+
+
+
+
+
+  // Need a function to hit user index route
+
+  getUser (jwtToken) {
+      let token = "Bearer " + jwtToken
+      console.log(token)
+      const email = this.state.loginEmail
+      fetch(baseURL + '/users', {
+        method: "GET",
+        headers: {
+      "Authorization": token
+    }
+      })
+      .then(response => response.json()).then((json) => {
+          // return this.setState({
+          // userMeals:json
+          // const user = json.filter(user => user.email === this.state.loginEmail)
+          // return console.log(user)
+          // // }
+          // )
+          console.log(this.state.loginEmail)
+          const user = json.filter(user => {
+           return  user.email === email
+          })
+          return console.log(user[0].id)
+        })
+      .catch(error => console.error(error))
+      
+
   }
   logOut (event) {
     event.preventDefault()
@@ -110,16 +152,16 @@ class App extends React.Component {
       <Router>
         <Navbar />
       
-      <div className = 'container'>
-        <Route path = '/login' render = {() => <Login login = {this.login} handleChange = {this.handleChange} loginEmail = {this.state.loginEmail} loginPassword = {this.state.loginPassword}/>} />
+        <div className = 'container'>
+          <Route path = '/login' render = {() => <Login login = {this.login} handleChange = {this.handleChange} loginEmail = {this.state.loginEmail} loginPassword = {this.state.loginPassword}/>} />
 
-<Route path ='/signup' render = {() => <NewUser handleAdd = {this.handleAdd} handleChange = {this.handleChange} email = {this.state.email} password = {this.state.password} password_confirmation = {this.state.password_confirmation}/> } />
+          <Route path ='/signup' render = {() => <NewUser handleAdd = {this.handleAdd} handleChange = {this.handleChange} email = {this.state.email} password = {this.state.password} password_confirmation = {this.state.password_confirmation}/> } />
 
-<button onClick = {this.logOut}>Log Out</button>
-        <h1>Some User Name Food Tracker</h1>
+          <button onClick = {this.logOut}>Log Out</button>
+          <h1>Some User Name Food Tracker</h1>
 
-        {/* this way - when logged in state, can render just home page or just sign up page */}
-        <Homepage />
+          {/* this way - when logged in state, can render just home page or just sign up page */}
+          <Homepage />
           
         
         
